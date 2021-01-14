e9176d939b0c7f4bbc5b43f7ccf1f9c80c24deb4
André Luiz Dias de Oliveira <51346021+Andr3zinh00@users.noreply.github.com>
2021-01-13 22:49:01 -0300

    First commit

---
+++ src/Servidor/Servidor.java
@@ -0,0 +1,11 @@
+package Servidor;
+
+/**
+ * @author André Luiz Dias de Oliveira
+ * matricula: 20181080080032
+ */
+public class Servidor {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().maxMemory());
+    }
+}
