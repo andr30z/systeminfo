febab6eb9fbeaee2390f3624cb024292f0f37179
André Luiz Dias de Oliveira <51346021+Andr3zinh00@users.noreply.github.com>
2021-01-17 00:09:57 -0300

    Finalização da atividade

---
+++ src/atividade/Client.java
@@ -0,0 +1,45 @@
+package atividade;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * @author André Luiz Dias de Oliveira
+ * Implementar um pequeno protocolo para coletar informações sobre a máquina remota (servidor).
+ * As informações devem incluir, Memória total, Memória usada, Memória livre,
+ * Número de processadores disponíveis, espaço total do disco principal e espaço livre do disco principal.
+ */
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 8080);
+        System.out.println("Menu:" +
+                "Digite 1 - Memoria Total\n" +
+                "Digite 2 - Memoria Livre\n" +
+                "Digite 3 - Memoria Usada\n" +
+                "Digite 4 - Numero de Processadores disponiveis\n" +
+                "Digite 5 - Espaço total em disco\n" +
+                "Digite 6 - Espaço disponivel em disco\n" +
+                "Digite 0 - Para sair"
+        );
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+        InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        while (true) {
+
+            String clientChoice = scanner.nextLine();
+            printWriter.println(clientChoice);
+            printWriter.flush();
+            String serverResponse = bufferedReader.readLine();
+            System.out.println(serverResponse);
+            if (clientChoice.equals("0")) {
+                socket.close();
+                break;
+            }
+        }
+    }
+}
---
+++ src/atividade/Servidor.java
@@ -0,0 +1,70 @@
+package atividade;
+
+
+import com.sun.management.OperatingSystemMXBean;
+
+import java.io.*;
+import java.lang.management.ManagementFactory;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author André Luiz Dias de Oliveira
+ */
+public class Servidor {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        Socket socket = serverSocket.accept();
+        System.out.println(socket.getInetAddress() + " Conectado!");
+        InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+        while (true) {
+            String clientResponse = bufferedReader.readLine();
+            if (clientResponse == null || clientResponse.equals("0"))
+                break;
+            printWriter.println(doSwitch(clientResponse));
+            printWriter.flush();
+        }
+        serverSocket.close();
+    }
+
+
+    public static String doSwitch(String response) {
+        switch (response) {
+            case "1":
+                return getSystemMemoryInfo(1);
+            case "2":
+                return getSystemMemoryInfo(2);
+            case "3":
+                return getSystemMemoryInfo(3);
+            case "4":
+                return String.valueOf(ManagementFactory.getOperatingSystemMXBean().getAvailableProcessors()) + " processadores";
+            case "5":
+                return convertResponse(new File("/").getTotalSpace());
+            case "6":
+                return  convertResponse(new File("/").getFreeSpace());
+            default:
+                return "Informe um valor valido!";
+
+        }
+    }
+
+    public static String convertResponse(long response) {
+        double converted = (double) response / 1024 / 1024 / 1024;
+        return String.valueOf(converted) +" GB";
+    }
+
+    public static String getSystemMemoryInfo(int memoryType) {
+        OperatingSystemMXBean os = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+        long memory = 0;
+        if (memoryType == 1)
+            memory = os.getTotalPhysicalMemorySize();
+        else if (memoryType == 2)
+            memory = os.getFreePhysicalMemorySize();
+        else
+            memory = os.getTotalPhysicalMemorySize() - os.getFreePhysicalMemorySize();
+        return convertResponse(memory);
+    }
+
+}
